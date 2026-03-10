82ce18277fd837ef535d35a1acc90584ffdc2472
lailachouraichi992-tech <lailachouraichi992@gmail.com>
2026-03-10 02:29:20 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./routes";
+import ReactDOM from "react-dom/client";
+import RoutesApp from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <RoutesApp />
   </BrowserRouter>
 );
