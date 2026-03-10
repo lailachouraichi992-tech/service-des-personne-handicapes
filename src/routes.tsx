ce6791184739adb9d827d0f875809bd7bcaeb6e4
lailachouraichi992-tech <lailachouraichi992@gmail.com>
2026-03-10 02:30:38 +0100

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,37 +5,28 @@ import Materieles from "./components/materieles";
 import Droits from "./components/droits";
 import Services from "./components/services";
 
-import Education from "./components/Education";
-import Sport from "./components/Sport";
-import Travail from "./components/Travail";
-import Natation from "./components/Natation";
-import Mobilite  from "./components/Mobilite";
+import Education from "./components/education";
+import Sport from "./components/sport";
+import Travail from "./components/travail";
+import Natation from "./components/natation";
+import Mobilite from "./components/mobilite";
 
-
-
-
-
-function routes() {
+function RoutesApp() {
   return (
     <Routes>
-
       <Route path="/" element={<Home />} />
-      <Route path="/Home" element={<Home />} />
-      <Route path="/Materieles" element={<Materieles />} />
-      <Route path="/Droits" element={<Droits />} />
-      <Route path="/Services" element={<Services />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/materieles" element={<Materieles />} />
+      <Route path="/droits" element={<Droits />} />
+      <Route path="/services" element={<Services />} />
 
-
-
-      <Route path="/Education" element={<Education />} />
-      <Route path="/Sport" element={<Sport />} />
-      <Route path="/Travail" element={<Travail />} />
-
-      <Route path="/Natation" element={<Natation />} />
-      <Route path="/Mobilite" element={<Mobilite />} />
-
+      <Route path="/education" element={<Education />} />
+      <Route path="/sport" element={<Sport />} />
+      <Route path="/travail" element={<Travail />} />
+      <Route path="/natation" element={<Natation />} />
+      <Route path="/mobilite" element={<Mobilite />} />
     </Routes>
   );
 }
 
-export default routes;
+export default RoutesApp;
